4cbc1ff88f8897c0cfc69baa4124df5fb0b0476c
Khaleel <01fe24ica06@kletech.ac.in>
2025-12-03 16:24:39 +0530

    Modified array

--- array_score.py
+++ array_score.py
@@ -1,10 +1,14 @@
-# Master Branch Code
-# Calculates sum and average of scores
+# ABC Branch Code
+# Calculates sum, average, maximum, and minimum of scores
 
 scores = list(map(int, input("Enter scores separated by spaces: ").split()))
 
 total = sum(scores)
 average = total / len(scores)
+maximum = max(scores)
+minimum = min(scores)
 
 print("Sum =", total)
 print("Average =", average)
+print("Maximum =", maximum)
+print("Minimum =", minimum)
