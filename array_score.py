636275fc4ffdd816ef16d608c84aff36fedfa7b7
Khaleel <01fe24ica06@kletech.ac.in>
2025-12-03 16:43:27 +0530

    Modified

--- array_score.py
+++ array_score.py
@@ -1,24 +1,9 @@
-# ABC Branch Code
-# Calculates sum, average, maximum, and minimum of scores
-# Jenkins-friendly version (no input())
-
 import sys
 
-# Check if scores are passed
-if len(sys.argv) < 2:
-    print("Error: No scores provided. Pass scores like:")
-    print('python array_score.py "10 20 30 40"')
-    sys.exit(1)
+scores_str = sys.argv[1].replace(",", " ")
+scores = list(map(int, scores_str.split()))
 
-# Read scores from command-line argument
-scores = list(map(int, sys.argv[1].split()))
-
-total = sum(scores)
-average = total / len(scores)
-maximum = max(scores)
-minimum = min(scores)
-
-print("Sum =", total)
-print("Average =", average)
-print("Maximum =", maximum)
-print("Minimum =", minimum)
+print("Sum =", sum(scores))
+print("Average =", sum(scores) / len(scores))
+print("Maximum =", max(scores))
+print("Minimum =", min(scores))
